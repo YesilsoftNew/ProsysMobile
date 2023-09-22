a7455250b6c6d14eba5b057f0a10f918cc489654
yasinaltintop <altintopyasin8@gmail.com>
2023-09-22 20:08:26 +0300

    PSL-51

---
+++ ProsysMobile/ProsysMobile/Helper/DefaultSettingsKey.cs
@@ -0,0 +1,8 @@
+namespace ProsysMobile.Helper
+{
+    public static class DefaultSettingsKey
+    {
+        public static string Language { get; set; } = "Language";
+        public static string DeviceGuid { get; set; } = "DeviceGuid";
+    }
+}
