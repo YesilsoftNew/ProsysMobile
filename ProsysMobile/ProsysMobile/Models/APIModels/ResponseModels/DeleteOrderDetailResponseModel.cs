7fd373ff552083acaa09428a14835515baf06d52
Yasin Altıntop <altintopyasin8@gmail.com>
2023-12-07 15:34:45 +0300

    PSL-131

---
+++ ProsysMobile/ProsysMobile/Models/APIModels/ResponseModels/DeleteOrderDetailResponseModel.cs
@@ -0,0 +1,8 @@
+namespace ProsysMobile.Models.APIModels.ResponseModels
+{
+    public class DeleteOrderDetailResponseModel
+    {
+        public int BasketItemCount { get; set; }
+        public string NetTotal { get; set; }
+    }
+}
